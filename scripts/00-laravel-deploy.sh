3d19094259146801d957389c145eb2ff5fbfc242
Hector <akpohector4@gmail.com>
2025-05-14 00:36:16 +0100

    last bof

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,5 +12,9 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Running seeds by force..."
-php artisan db:seed --force
+echo "Setting permissions for images directory..."
+sudo chown -R www-data:www-data /var/www/html/public/images
+sudo chmod -R 755 /var/www/html/public/images
+
+# echo "Running seeds by force..."
+# php artisan db:seed --force
